46f7f717b3a2240a7957fdba4a0db1f237878349
Alex Geier <alexjgeier@gmail.com>
2018-04-09 23:11:40 -0400

    added readme

---
+++ README.md
@@ -0,0 +1,11 @@
+Names:
+Sanchit Gavankar sgavanka@purdue.edu
+Alex Geier ageier@purdue.edu
+Paramesh Pradeep pradeepp@purdue.edu
+
+Project Name and Description:
+
+
+Technologies:
+JavaScript, HTML, FirebAse
+                                  
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
-alert("hello");
+writeToDatabase = function() {
+    var fileList = firebase.database().ref().child('users');
+    var newFile = fileList.push(); // generate a new fileID
+    newFile.set({
+        'fileName': "hello",
+        'fileContents': 'word'
+    });
+}
