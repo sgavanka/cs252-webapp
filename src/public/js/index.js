2df341c8c1695af47b8e67cf18ccbf6c5e1c5c67
sgavanka <sgavanka@purdue.edu>
2018-04-12 21:21:35 -0400

    merged

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,13 +1,32 @@
 let loginButton;
 let registerButton;
+let loginEmail;
+let loginPassword;
 
 window.onload = function() {
+    loginEmail = document.getElementById("login-email");
+    loginPassword = document.getElementById("login-password");
     loginButton = document.getElementById("login-button");
     registerButton = document.getElementById("register-button");
     loginButton.addEventListener('click', function() {
-        window.location.assign("login.html");
+        login();
     });
     registerButton.addEventListener('click', function() {
         window.location.assign("register.html");
+    });
+}
+
+login = function() {
+    firebase.auth().signInWithEmailAndPassword(loginEmail.value, loginPassword.value).then(function() {
+        // // If login successful, redirect the user to the main.html page
+        if (firebase.auth().currentUser) {
+            window.location.assign("main.html");
+        } else {
+            alert("Login failed");
+        }
+    }).catch(function(error) {
+        if (error != null) {
+            alert(error.message);
+        }
     });
 }
